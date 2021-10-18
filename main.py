dc83c8d048926a2e332ed693498ec91aa04c53b9
Николай Заболотнов <z.nik.vladim.312@gmail.com>
2021-10-18 15:01:20 +0300

    Added logging into main.py and rating_check module

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
 import telebot
 import rating_check
+import logging
+
 from auth_data import token
 
 
-def telegram_bot(token):
+def main():
+    logger = logging.getLogger('Bot')
+    logger.setLevel(logging.INFO)
+
+    fh = logging.FileHandler("bot.log")  # file handler для логов
+
+    # формат записи логов
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+
+    # добавляем file handler к объекту логов
+    logger.addHandler(fh)
+
+    # запуск бота
+    telegram_bot(token, logger)
+    logger.info('Program started')
+
+
+def telegram_bot(token, logger):
     bot = telebot.TeleBot(token)
 
     @bot.message_handler(commands=["start"])
     def start_message(message):
         bot.send_message(message.chat.id, "Привет")
+        logger.info('Bot sent start message')
 
     @bot.message_handler(content_types=["text"])
-    def send_text(message):
+    def send_reply(message):
+        logger.info('Bot received message')
         if message.text.lower() == "рейтинг гаряева":
             try:
+                logger.info('Bot received check rating updates command')
                 rating_check.check_rating_updates(bot, message)
             except Exception as ex:
+                logger.error(ex)
                 print(ex)
                 bot.send_message(
                     message.chat.id,
@@ -24,6 +48,9 @@ def telegram_bot(token):
 
     bot.polling()
 
+# TODO логгирование
+# TODO клавиатура
+
 
 if __name__ == '__main__':
-    telegram_bot(token)
+    main()
--- rating_check.py
+++ rating_check.py
@@ -3,8 +3,11 @@ import filecmp
 import os
 import PyPDF2
 import shutil
+import logging
 
 from urllib.parse import urlencode
+
+# module_logger = logging.getLogger('Bot.rating_check')
 
 
 def parse_mod_date(filename, separator=None):
@@ -15,7 +18,7 @@ def parse_mod_date(filename, separator=None):
     :return: форматированная дата модификации файла
     """
     with open(filename, "rb") as pdf:
-        pdf_reader = PyPDF2.PdfFileReader(pdf)  # "скармливаем PdfFileReader наш PDF файл
+        pdf_reader = PyPDF2.PdfFileReader(pdf)  # "скармливаем" PdfFileReader наш PDF файл
         mod_date = pdf_reader.documentInfo["/ModDate"]  # вытаскиваем из метаданных файла дату последнего изменения
         # урезаем лишнюю информацию из mod_date
         mod_date = mod_date[2:]
@@ -46,6 +49,7 @@ def check_rating_updates(bot, message):
     :param message: объект сообщения пользователя
     :return:
     """
+    logger = logging.getLogger("Bot.rating_check.check_rating_updates")
 
     base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'  # URL для загрузки с Я.Диска
     public_key = 'https://disk.yandex.ru/i/n1KicdZ1pObPIg'  # ссылка на конкретный файл
@@ -56,28 +60,39 @@ def check_rating_updates(bot, message):
 
     download_response = requests.get(download_url)  # записываем содержимое файла с диска
 
+    # TODO подумать над созданием отдельной папки для работы модуля проверки обновления рейтинга
     # if not os.path.isdir('rating_data'):
     #     os.mkdir('rating_data')
     #     data_path = 'rating_data'
 
     with open('oppr_new.pdf', "wb") as downloaded_file:
         downloaded_file.write(download_response.content)  # сохраняем в файл содержимое файла с диска
+        logger.info("Downloaded file from Yandex.Disk")
 
     if filecmp.cmp('oppr_new.pdf', 'oppr_old.pdf', shallow=True):  # сравниваем содержимое файлов - старого и нового
         # если изменений нет, то пишем сообщение пользователю с датой последнего изменения и ссылкой на файл
-        return bot.send_message(message.chat.id, f"Нет изменений с {parse_mod_date('oppr_old.pdf')} "
+        bot.send_message(message.chat.id, f"Нет изменений с {parse_mod_date('oppr_old.pdf')} "
                                                  f"\nСсылка на файл {public_key}")
+        logger.info("Bot sent no changes reply")
     else:
         # если изменения есть, то пишем сообщение пользователю с датой изменения старого файла и нового + ссылка на файл
         bot.send_message(message.chat.id, f"Есть изменения c {parse_mod_date('oppr_old.pdf')}, "
                                           f"изменения внесены {parse_mod_date('oppr_new.pdf')} "
                                           f"\nСсылка на файл {public_key}")
+        logger.info("Bot sent changes reply")
+
         # создаем папку для архива старых файлов с рейтингов и перемещаем туда файл,
         # который после проверки считается устаревшим
         if os.path.isdir('rating_archive'):
+            logger.info("oppr_old.pdf archieved in rating_archive")
             shutil.move('oppr_old.pdf', f'rating_archive/oppr_old_{parse_mod_date("oppr_old.pdf", "_")}.pdf')
+            logger.info("New file renamed as old")
             os.rename('oppr_new.pdf', 'oppr_old.pdf')
         else:
+            logger.warning("Missing rating_archive directory")
             os.mkdir('rating_archive')
+            logger.info("rating_archive directory created")
+            logger.info("oppr_old.pdf archieved in rating_archive")
             shutil.move('oppr_old.pdf', f'rating_archive/oppr_old_{parse_mod_date("oppr_old.pdf", "_")}')
+            logger.info("New file renamed as old")
             os.rename('oppr_new.pdf', 'oppr_old.pdf')
