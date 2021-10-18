67d0a28efbc9bcdc70ec28c2c8b475f2a8525603
Николай Заболотнов <z.nik.vladim.312@gmail.com>
2021-10-18 03:05:53 +0300

    Created tg bot, added check rating bot command, modified check rating module

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+import telebot
+import rating_check
+from auth_data import token
 
+
+def telegram_bot(token):
+    bot = telebot.TeleBot(token)
+
+    @bot.message_handler(commands=["start"])
+    def start_message(message):
+        bot.send_message(message.chat.id, "Привет")
+
+    @bot.message_handler(content_types=["text"])
+    def send_text(message):
+        if message.text.lower() == "рейтинг гаряева":
+            try:
+                rating_check.check_rating_updates(bot, message)
+            except Exception as ex:
+                print(ex)
+                bot.send_message(
+                    message.chat.id,
+                    "Ошибка"
+                )
+
+    bot.polling()
+
+
+if __name__ == '__main__':
+    telegram_bot(token)
--- rating_check.py
+++ rating_check.py
@@ -1,23 +1,80 @@
 import requests
 import filecmp
+import os
+import PyPDF2
 
 from urllib.parse import urlencode
 
 
-def check_rating_updates():
-    base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'
-    public_key = 'https://disk.yandex.ru/i/n1KicdZ1pObPIg'
+def parse_mod_date(filename, separator=None):
+    """
+    Функция для парсинга и форматирования даты модификации PDF файла
+    :param separator: используемый сепаратор для разделения частей даты
+    :param filename: имя PDF файла
+    :return: форматированная дата модификации файла
+    """
+    with open(filename, "rb") as pdf:
+        pdf_reader = PyPDF2.PdfFileReader(pdf)
+        mod_date = pdf_reader.documentInfo["/ModDate"]
+        # урезаем лишнюю информацию из mod_date
+        mod_date = mod_date[2:]
+        mod_date = mod_date[:14]
+        # date
+        year = mod_date[:4]
+        month = mod_date[4:6]
+        date = mod_date[6:8]
+        # time
+        hour = mod_date[8:10]
+        minutes = mod_date[10:12]
+        seconds = mod_date[12:14]
+        if separator == None:
+            parsed_mod_date = f"{date}.{month}.{year} {hour}:{minutes}:{seconds}"
+        else:
+            parsed_mod_date = f"{date}{separator}{month}{separator}{year}{separator}" \
+                              f"{hour}{separator}{minutes}{separator}{seconds}"
+    return parsed_mod_date
 
-    final_url = base_url + urlencode(dict(public_key=public_key))
-    response = requests.get(final_url)
-    download_url = response.json()['href']
 
-    download_response = requests.get(download_url)
+def check_rating_updates(bot, message):
+    """
+    Функция для проверки обновления в рейтинговой таблице Гаряева
+    :param bot: объект бота от которого отправляется сообщение
+    :param message: объект сообщения пользователя
+    :return:
+    """
 
-    with open('oppr_new.pdf', "wb") as f:
-        f.write(download_response.content)
+    base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'  # URL для загрузки с Я.Диска
+    public_key = 'https://disk.yandex.ru/i/n1KicdZ1pObPIg'  # ссылка на конкретный файл
 
-    if filecmp.cmp('oppr_new.pdf', 'oppr_old.pdf', shallow=True):
-        print("Нет изменений")
+    final_url = base_url + urlencode(dict(public_key=public_key))  # создаем ссылку на по которой отправим get-запрос
+    response = requests.get(final_url)  # посылаем get-запрос
+    download_url = response.json()['href']  # получаем загрузочную ссылку
+
+    download_response = requests.get(download_url)  # записываем содержимое файла с диска
+
+    # if not os.path.isdir('rating_data'):
+    #     os.mkdir('rating_data')
+    #     data_path = 'rating_data'
+
+    with open('oppr_new.pdf', "wb") as downloaded_file:
+        downloaded_file.write(download_response.content)  # сохраняем в файл содержимое файла с диска
+
+    if filecmp.cmp('oppr_new.pdf', 'oppr_old.pdf', shallow=True):  # сравниваем содержимое файлов - старого и нового
+        # если изменений нет, то пишем сообщение пользователю с датой последнего изменения и ссылкой на файл
+        return bot.send_message(message.chat.id, f"Нет изменений с {parse_mod_date('oppr_old.pdf')} "
+                                                 f"\nСсылка на файл {public_key}")
     else:
-        print("Есть изменения")
+        # если изменения есть, то пишем сообщение пользователю с датой изменения старого файла и нового + ссылка на файл
+        bot.send_message(message.chat.id, f"Есть изменения c {parse_mod_date('oppr_old.pdf')}, "
+                                          f"изменения внесены {parse_mod_date('oppr_new.pdf')} "
+                                          f"\nСсылка на файл {public_key}")
+        # создаем папку для архива старых файлов с рейтингов и перемещаем туда файл,
+        # который после проверки считается устаревшим
+        # TODO Пофиксить баг с ошибкой при перемещении файла в архив
+        if os.path.isdir('rating_archive'):
+            os.rename('oppr_old.pdf', f'/rating_archive/oppr_old_{parse_mod_date("oppr_old.pdf", "_")}.pdf')
+            os.rename('oppr_new.pdf', 'oppr_old.pdf')
+        else:
+            os.mkdir('rating_archive')
+            os.rename('oppr_old.pdf', f'/rating_archive/oppr_old_{parse_mod_date("oppr_old.pdf", "_")}')
+            os.rename('oppr_new.pdf', 'oppr_old.pdf')
